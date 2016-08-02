6352f5800d576a0c784f3784514736af186b99cf
JMichalowski <jk3105@wp.pl>
2016-08-02 09:49:11 +0200

    test

---
+++ app/src/main/java/pl/droidsonroids/hodor/Constants.java
@@ -0,0 +1,8 @@
+package pl.droidsonroids.hodor;
+
+public class Constants {
+
+    public static final String DB_USER = "users";
+    public static final String DB_FRIENDS = "friends";
+    public static final String DB_TOKEN = "token";
+}
